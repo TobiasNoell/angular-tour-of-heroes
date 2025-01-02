3aa95de3e894011c33eecc5f190ea808f72d8740
Tobias <tobias.noell@colormass.com>
2025-01-02 17:42:22 +0100

    19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule],
+  standalone: true,
 })
 export class AppComponent {
   title = 'angular-tour-of-heroes';
